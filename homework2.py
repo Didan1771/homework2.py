5ea81a02c9fdf522c4ce168f85b30bdf44761c39
Didan1771 <didan1771@icloud.com>
2024-05-23 22:14:43 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -2,4 +2,5 @@
 homeworks_done='12,'
 time='1,5,'
 name_study='Python,'
-print('Курс:', name_study, 'всего задач:', homeworks_done,'затрачено часов:',time,'среднее время выполнения:', 15/120)
+time_work='0,125'
+print('Курс:', name_study, 'всего задач:', homeworks_done,'затрачено часов: 1,5, среднее время выполнения ',time_work,' часа')
